38be8ab78cdd70c9763d367a4cd8686814e4228d
octavia <Ogomes21@gmail.com>
2022-07-30 13:27:01 -0400

    Made program.

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,46 @@
 package io.zipcoder;
 
 
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+        int numPets;
+
+        //Asks user how many pets they have
+        System.out.println("How many pets do you have?");
+        Scanner userInput = new Scanner(System.in);
+        numPets = userInput.nextInt();
+        Pet[] userPets = new Pet[numPets];
+        //User picks type and set a name
+        for (int i =0; i < numPets; i++) {
+            System.out.println("What is pet #" + (i+1) + "'s type? (Dog, Cat, or Pig?)");
+            String petType = userInput.next().toLowerCase();
+            String petName;
+            switch (petType) {
+                case "dog":
+                    System.out.println("What is your dog's name?");
+                    petName = userInput.next();
+                    userPets[i] = new Dog(petName);
+                    break;
+                case "cat":
+                    System.out.println("What is your cat's name?");
+                    petName = userInput.next();
+                    userPets[i] = new Cat(petName);
+                    break;
+                case "pig":
+                    System.out.println("What is your pig's name?");
+                    petName = userInput.next();
+                    userPets[i] = new Pig(petName);
+                    break;
+                default:
+                    i--;
+                    break;
+            }
+
+        }
+        //Will print all pets
+        for (Pet pet : userPets) System.out.print(pet);
+    }
 }
---
+++ src/main/java/io/zipcoder/Pig.java
@@ -0,0 +1,15 @@
+package io.zipcoder;
+
+public class Pig extends Pet{
+
+
+public Pig(String name) {
+    super(name);
+}
+
+@Override
+public String speak() {
+    return "Oink!";
+}
+}
+
